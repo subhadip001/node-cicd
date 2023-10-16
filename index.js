ac7a06c29123aae7c806c7b6a6b282832271b66e
Subhadip <mnhacker2001@gmail.com>
2023-10-17 02:31:40 +0530

    added post route

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World!" });
+});
+
+app.post("/name", (req, res) => {
+  const { name } = req.body;
+  console.log(req.body);
+  res.status(200).json({ message: `Hello ${name}` });
 });
 
 app.listen(PORT, () => {
