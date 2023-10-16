a500255b4a8afa5859e3ef4d338c943b7b6aedb9
Subhadip <mnhacker2001@gmail.com>
2023-10-17 00:10:58 +0530

    cicd checking

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World!" });
 });
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
+app.listen(PORT, () => {
+  console.log(`Server is running on port 8080${PORT}`);
 });
